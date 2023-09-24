f82c09d70555cfe9bb3f72f02dfc3da1ec637bc3
AbuSolaimane <fadilimustafa530@gmail.com>
2023-09-24 09:58:39 -0700

    hjdjd

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,14 @@
-#!/usr/bin/env bash
-#connect to my remote server using SSH
-ssh -i ~/.ssh/school ubuntu@107.23.107.180
+# a Puppet Script to change ssh config file(/etc/ssh/ssh_config)
+# in the server
+
+file_line { 'forbid passwd auth':
+  path   => '/etc/ssh/ssh_config',
+  ensure => 'present',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'change file to identify':
+  path   => '/etc/ssh/ssh_config',
+  ensure => 'present',
+  line   => '    IdentityFile ~/.ssh/school',
+}
